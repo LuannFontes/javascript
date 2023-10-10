a81ca44c7fd6816449229e44e99e9518f336cad0
luannfontes <136660859+LuannFontes@users.noreply.github.com>
2023-10-10 17:21:52 -0300

    conclusão do ex14

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,12 +1,22 @@
+function carregar() {
+
+
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
-msg.innerHTML = (`Agoroa são ${hora}`horas)
-if (hora >= && hora < 12) {
+msg.innerHTML = `Agora são ${hora} horas`
+if (hora >= 0 && hora < 12) {
     //BOM DIA
-} else if (hora >=12 && hora < 18) {
+    img.src = "fotomanha.png"
+    document.body.style.background = '#e2cd9f'
+} else if (hora >=12 && hora <= 18) {
     //BOA TARDE
+    img.src = "fototarde.png"
+    document.body.style.background = '#b9846f'
 } else {
     //BOA NOITE
+    img.src = "fotonoite.png"
+    document.body.style.background = '#515154' 
+}
 }
